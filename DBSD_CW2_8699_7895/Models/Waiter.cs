69682be60feb6d707a57e62c8d0ef9ba4543eadb
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 15:11:02 +0500

    added waiter model

--- DBSD_CW2_8699_7895/Models/Waiter.cs
+++ DBSD_CW2_8699_7895/Models/Waiter.cs
@@ -7,11 +7,17 @@ namespace DBSD_CW2_8699_7895.Models
     {
         [DisplayName("WaiterId")]
         public int? WaiterId { get; set; }
+        [DisplayName("FirstName")]
         public string FirstName { get; set; }
+        [DisplayName("LastName")]
         public string LastName { get; set; }
+        [DisplayName("DoB")]
         public DateTime DoB { get; set; }
+        [DisplayName("Salary")]
         public int? Salary { get; set; }
+        [DisplayName("PhoneNumber")]
         public string PhoneNumber { get; set; }
+        [DisplayName("RestaurantId")]
         public int? RestaurantId  {get;set;}
         public Restaurant Restaurant { get; set; }
     }
