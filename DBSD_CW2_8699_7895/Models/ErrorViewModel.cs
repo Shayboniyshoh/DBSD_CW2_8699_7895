a29771a5406c026eef5980b87f0b81073321f3dc
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 13:02:31 +0500

    Add project files.

---
+++ DBSD_CW2_8699_7895/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBSD_CW2_8699_7895.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
