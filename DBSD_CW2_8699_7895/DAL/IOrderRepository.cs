53cec356a1ca8bd32af2dbe7bd8b924597d12be2
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 17:19:20 +0500

    data access layer added without CRUD logic for now

---
+++ DBSD_CW2_8699_7895/DAL/IOrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using DBSD_CW2_8699_7895.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DBSD_CW2_8699_7895.DAL
+{
+    public interface IOrderRepository
+    {
+        public List<Order> GetOrders();
+        public List<Order> Filter(
+              string pizzaName,
+              DateTime orderDate,
+              string size,
+              decimal discount,
+              int restaurantId,
+              string sortColumn,
+              bool sortDesc = false,
+              int? page = 1,
+              int pageSize = 2
+          );
+
+        public void Insert(Order item);
+
+        public void Update(Order item);
+
+        public Order GetById(int id);
+
+        public void Delete(int id);
+    }
+
+}
+}
---
+++ DBSD_CW2_8699_7895/DAL/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿using DBSD_CW2_8699_7895.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DBSD_CW2_8699_7895.DAL
+{
+    public class OrderRepository : IOrderRepository
+    {
+
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> Filter(string pizzaName, DateTime orderDate, string size, decimal discount, int restaurantId, string sortColumn, bool sortDesc = false, int? page = 1, int pageSize = 2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Order item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
