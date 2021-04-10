52102637b16accd669ba8dbed8ad0d72b1dfbfc7
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 15:12:11 +0500

    added chef model

---
+++ DBSD_CW2_8699_7895/Models/Chef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DBSD_CW2_8699_7895.Models
+{
+    public class Chef
+    {
+        [DisplayName("ChefId")]
+        public int? ChefId { get; set; }
+        [DisplayName("FirstName")]
+        public string FirstName { get; set; }
+        [DisplayName("LastName")]
+        public string LastName { get; set; }
+        [DisplayName("DoB")]
+        public DateTime DoB { get; set; }
+        [DisplayName("Salary")]
+        public int? Salary { get; set; }
+        [DisplayName("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+        [DisplayName("RestaurantId")]
+        public int? RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+    }
+}
