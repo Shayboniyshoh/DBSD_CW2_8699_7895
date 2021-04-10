fa0da20f8ebd5e1a9d2dcf54ec30a249ff905caf
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 15:15:21 +0500

    updated restaurant model

--- DBSD_CW2_8699_7895/Models/Restaurant.cs
+++ DBSD_CW2_8699_7895/Models/Restaurant.cs
@@ -5,7 +5,7 @@ namespace DBSD_CW2_8699_7895.Models
     public class Restaurant
     {
         [DisplayName("RestaurantId")]
-        public int? RestaurantId { get; set; }
+        public int RestaurantId { get; set; }
         [DisplayName("[Name]")]
         public string Name { get; set; }
         [DisplayName("Street")]
