6fce77ea4d7366cbe559dfda8faead15c84a4c4d
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-04-10 15:07:26 +0500

    added Restaurant model

---
+++ DBSD_CW2_8699_7895/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace DBSD_CW2_8699_7895.Models
+{
+    public class Restaurant
+    {
+        [DisplayName("RestaurantId")]
+        public int? RestaurantId { get; set; }
+        [DisplayName("[Name]")]
+        public string Name { get; set; }
+        [DisplayName("Street")]
+        public string Street { get; set; }
+        [DisplayName("City")]
+        public string City { get; set; }
+    }
+}
